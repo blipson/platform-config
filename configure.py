3739845922635868c01e028340f90a4378c604b5
Ben Lipson <bjlipson@spscommerce.com>
2017-05-31 15:13:48 -0500

    Dynamically get token and stuff

--- configure.py
+++ configure.py
@@ -1,48 +1,80 @@
 import requests
 import json
+import getpass
+import datetime
+import time
 
-def main():
-    with open('test.json') as data_file:
-        data = json.load(data_file)
+url = f"https://dev.id.spsc.io/identity/"
 
-    payload = {
-        'created_at': data['created_at'],
-        'id': data['id'],
-        'status': data['status'],
-        'in_maintenance_mode': data['in_maintenance_mode'],
-        'launchpad_display': data['launchpad_display'],
-        'name': data['name'],
-        'url': data['url'],
-        'description': data['description'],
-        'namespace': data['namespace'],
-        'language_list': data['language_list'],
-        'version': data['version'],
-        'is_archived': data['is_archived'],
-        'is_service': data['is_service'],
-        'is_public': data['is_public'],
-        'logo': data['logo'],
-        'slug_archive': data['slug_archive'],
-        'language_version': data['language_version'],
-        'maintenance_url': data['maintenance_url'],
-        'slug': data['slug'],
-        'permissions': data['permissions'],
-        'publisher_org_id': data['publisher_org_id'],
-        'publisher_user_id': data['publisher_user_id'],
-        'roles': data['roles'],
-        'updated_at': data['updated_at']
+def get_token():
+    pw = getpass.getpass()
+    token_payload = {
+        'grant_type': 'password',
+        'username': 'bjlipson@spscommerce.com',
+        'client_id': 595,
+        'password': pw
     }
-    headers = {
-        'AUTHORIZATION': 'Bearer <TOKEN_GOES_HERE>',
+
+    token_headers = {
         'Content-Type': 'application/json;charset=UTF-8'
     }
 
-    url = f"https://dev.id.spsc.io/identity/v2/applications/{data['id']}/"
+    token_url = url + "token/"
+    r = requests.post(token_url, data=json.dumps(token_payload), headers=token_headers)
+    return r.json()
+
+
+def main():
+    res = get_token()
+
+    with open('test.json') as data_file:
+        data = json.load(data_file)
+    headers = {
+        'AUTHORIZATION': 'Bearer ' + res['access_token'],
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+    url = "https://dev.id.spsc.io/identity/" + f"v2/applications/{data['id']}/"
+    appData = requests.get(url, headers=headers)
+    print(appData)
+    appData = appData.json()
+    print(appData['created_at'])
+
+    for key in appData:
+        print(key + ": " + str(appData[key]))
+
+    version = 59
+    payload = {
+        'created_at': appData['created_at'],
+        'id': appData['id'],
+        'status': appData['status'],
+        'in_maintenance_mode': appData['in_maintenance_mode'],
+        'launchpad_display': appData['launchpad_display'],
+        'name': appData['name'],
+        'url': data['url'].replace('*', str(version)),
+        'description': appData['description'],
+        'namespace': appData['namespace'],
+        'language_list': appData['language_list'],
+        'version': version,
+        'is_archived': appData['is_archived'],
+        'is_service': appData['is_service'],
+        'is_public': appData['is_public'],
+        'logo': appData['logo'],
+        'slug_archive': appData['slug_archive'],
+        'language_version': appData['language_version'],
+        'maintenance_url': appData['maintenance_url'],
+        'slug': appData['slug'],
+        'permissions': data['permissions'],
+        'publisher_org_id': appData['publisher_org_id'],
+        'publisher_user_id': appData['publisher_user_id'],
+        'roles': appData['roles'],
+        'updated_at': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%SZ')
+    }
 
     print(f"PUTTING TO: {url}")
     print(f"WITH DATA: {json.dumps(payload)}")
     print(f"WITH HEADERS: {headers}")
     r = requests.put(url, data=json.dumps(payload), headers=headers)
 
-    print(r)
+    print(r.json())
 
 main()
